11578fb7902ff3b2c94108778fc2a07fc30ecaf5
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2017-12-17 16:03:29 +0100

    Modify scripts.js

--- scripts.js
+++ scripts.js
@@ -9,12 +9,12 @@ Phone.prototype.printInfo = function() {
 }
 
 var iPhone6S = new Phone("Apple", 2250, "silver");
-var Galaxy5S = new Phone("Samsung", 1200, "silver");
-var One = new Phone("OnePlus", 800, "white");
+var galaxy5S = new Phone("Samsung", 1200, "silver");
+var one = new Phone("OnePlus", 800, "white");
 
 iPhone6S.printInfo();
-Galaxy5S.printInfo();
-One.printInfo();
+galaxy5S.printInfo();
+one.printInfo();
 
 //Task upgrade
 
@@ -26,7 +26,7 @@ function Phone(brand, price, color, width, system) {
 	this.system = system;
 }
 
-Phone.prototype.Case = function() {
+Phone.prototype.case = function() {
 	if (this.system == "Android" && this.width >= 350) {
 		var sysUpperCase = this.system.toUpperCase();
 		console.log(sysUpperCase);
@@ -36,10 +36,34 @@ Phone.prototype.Case = function() {
 }
 
 var iPhone6S = new Phone("Apple", 2250, "silver", 350, "iOS");
-var Galaxy5S = new Phone("Samsung", 1200, "silver", 400, "Android");
-var One = new Phone("OnePlus", 800, "white", 300, "Android");
+var galaxy5S = new Phone("Samsung", 1200, "silver", 400, "Android");
+ 
 
-Galaxy5S.Case();
-One.Case();
+galaxy5S.Case();
+one.Case();
 iPhone6S.Case();
+
+//Apply discount
+/*Dopisz do telefonu funkcję applyDiscount z parametrem, która 
+będzie obniżać cenę telefonu. Funkcja ma modyfikować cenę telefonu 
+w taki sposób, że wartość telefonu zostanie obniżona o podaną kwotę 
+podaną jako parametr. Aby sprawdzić poprawność działania kodu, wypisz 
+dane telefonu, zastosuj obniżkę i ponownie wypisz dane telefonu. 
+Powinny pokazać się 2 różne wartości.
+Generalnie rzecz ujmując, po wywołaniu odpowiednio poleceń printInfo, 
+applyDiscount i ponownie printInfo, cena telefonu powinna ulec zmianie:
+var phone = new Phone(…, 1000, …); // ustawiamy cenę na 1000
+phone.printInfo(); // price = 1000
+phone.applyDiscount(100); // nic się nie wypisuje
+phone.printInfo(); // price = 900
+*/
+one.printInfo();
+
+Phone.prototype.applyDiscount = function(discount) {
+	this.price = this.price - discount;
+	return this.price;
+}
+
+one.applyDiscount(100);
+one.printInfo();
 
